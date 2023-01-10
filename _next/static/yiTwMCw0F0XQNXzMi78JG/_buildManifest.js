4039bfd07ebd5244775f4abdc22cc1b2ae0c311d
AQUASINE <42610534+AQUASINE@users.noreply.github.com>
2023-01-10 18:03:56 +0000

    Deploying to public from @ glumbocorp/glumbo-web-client@826235fb96bcceb53b13f5f8d098e853e43eece6 🚀

---
+++ _next/static/yiTwMCw0F0XQNXzMi78JG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/04700365d10cca5a.css","static/chunks/pages/index-1452a22a9b4905a0.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
